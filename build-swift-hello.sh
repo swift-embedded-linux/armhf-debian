993fc22ad399844fb2778342584dfad9c92f0562
Jesse L. Zamora <xtremekforever@gmail.com>
2026-02-15 13:27:34 +0000

    Cleanup build-swift-hello.sh to make variables shared, deploy static swift-hello in artifacts

--- build-swift-hello.sh
+++ build-swift-hello.sh
@@ -2,7 +2,5 @@
 set -e
 source swift-define
 
-# Build paths
-export SWIFT_PACKAGE_SRCDIR=$SRC_ROOT/swift-hello
-export SWIFT_PACKAGE_BUILDDIR=$SRC_ROOT/build/swift-hello-$SWIFT_TARGET_ARCH
+# Build the package
 ./build-swift-package.sh
